a41cf3369f4a10493bebe494da222a70645ab538
azuraga <73472335+azuraga@users.noreply.github.com>
2021-11-08 16:39:37 +0100

    added typescript

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import styles from "./Button.module.scss"
 
-export default function Button(props) {
+export default function Button() {
     return <button className={styles.button}>
         <img className={styles.image} alt="next"/>
     </button>
