118edb8961ae31dc837705e3f05b894375778d26
azuraga <73472335+azuraga@users.noreply.github.com>
2021-11-08 16:44:24 +0100

    added typescript

---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.scss'
+import Button from "../components/Button/Button";
+
+const Home: NextPage = () => {
+  return (
+      <div className={styles.container}>
+        <Head>
+          <title>Tadano Rei Birthday Project</title>
+          <meta name="description" content="Happy birthday!" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className={styles.main}>
+          <Button></Button>
+        </main>
+
+        <footer className={styles.footer}>
+          <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+          </a>
+        </footer>
+      </div>
+  )
+}
+
+export default Home
