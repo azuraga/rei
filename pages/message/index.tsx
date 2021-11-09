ca31e982b15578f8706ff7e87c2ad7cb99c60b9c
azuraga <73472335+azuraga@users.noreply.github.com>
2021-11-09 17:28:01 +0100

    Optimized text

--- pages/message/index.tsx
+++ pages/message/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Zoom} from "@mui/material";
+import {Container, Fade, Typography, Zoom} from "@mui/material";
 import styles from "../../styles/Home.module.scss";
 import NextButton from "../../components/Button/NextButton";
 
@@ -16,12 +16,40 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Zoom in={true}>
-                <main className={styles.main}>
+            <Fade in={true} timeout={3000}>
+                <Container>
                     <img className={styles.logo} src="/logo.png" alt="Tadano Rei Birthday Project"/>
-                    <NextButton href="/messages"/>
-                </main>
-            </Zoom>
+                </Container>
+            </Fade>
+
+            <main className={styles.main}>
+                <Container maxWidth="md">
+                    <Typography variant="body1" align="center">
+                        3周年おめでとうございます。
+                        <br/><br/>
+                        これほど長い期間にわたって活動しているVTuberはそうそういないでしょうね。あなたの最初の中途半端な動画から、今の本気と情熱を込めている配信まで見ることができて光栄でした。この素敵な日を迎えられたのはあなたの献身と努力の賜物です。
+                        <br/><br/>
+                        この数年間、あなたは視聴者さんに数え切れないほどの笑顔を下さっていました。困っている人がいても、ただ面白い場所が欲しい人がいても、あなたはいつも誰でもを迎え入れ、みんなのために温かい居場所を作ろうと努力しました。
+                        <br/><br/>
+                        当然毎日が楽ではありませんでした。苦労や延期など、終わりのないようなことが沢山ありました。それでもあなたは力強く全部耐え、最大限に我慢し、みんなを幸せにするために力を尽くしました。
+                        <br/><br/>
+                        あなたの存在としてくれていることへの感謝の気持ちを伝えるのは不可能だと思います。それでも、このプレゼントには私達の気持ちを精一杯詰め込みました。三月にあなたが私達に心を開いてくれたように、今回私達の本音をお伝えさせてください。あなたのことを大切に思っている私達の手紙ちを読んで、それを心にどこかで留めてくれるのなら非常に嬉しいです。
+                        <br/><br/>
+                        一生懸命頑張ってくれてありがとう。
+                        いつも自分らしく居ていてくれてありがとう。
+                        夢を諦めないでありがとう。
+                        笑顔を元気をいつも届けてくれてありがとう。
+                        世界一のVtuberになってくれてありがとう。
+                        <br/><br/>
+                        Reiyansであることは私達にとって最高の名誉です。
+                        <br/><br/>
+                        これからもあなたのそばにいて、夢を叶えさせてください。
+                        <br/><br/><br/><br/>
+                        Reiyansより
+                    </Typography>
+                </Container>
+            </main>
+            <NextButton href="/messages"/>
 
         </div>
     )
