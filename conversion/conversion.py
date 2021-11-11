5ea5acb22a253bec44e39a8751faa47dcded0f9a
azuraga <73472335+azuraga@users.noreply.github.com>
2021-11-11 21:05:04 +0100

    added preprocessing method for messages

---
+++ conversion/conversion.py
@@ -0,0 +1,25 @@
+import json
+import csv
+
+
+def csv_to_json(filename):
+    """
+    Converts the CSV file of this project to an organised JSON file. Only considers messages and their author,
+     thus fanart and timestamp are not included.
+    """
+    result = []
+    with open('{}.csv'.format(filename), newline='\n') as file:
+        reader = csv.reader(file, delimiter=',')
+        for ix, row in enumerate(reader):
+            if ix == 0:  # skip header
+                continue
+            result.append({
+                'author': row[0],
+                'message': {
+                    'EN': row[2],
+                    'JA': row[3]
+                }
+            })
+
+    with open('{}.json'.format(filename), 'w') as file:
+        file.write(json.dumps({'data': result}, indent=4))
