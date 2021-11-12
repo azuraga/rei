3c778c5e79e2619f91d4420f43ba3dff924c9e74
azuraga <73472335+azuraga@users.noreply.github.com>
2021-11-12 18:12:56 +0100

    added 'no translation' message | refactoring

--- components/MessageCard/MessageCard.tsx
+++ components/MessageCard/MessageCard.tsx
@@ -1,14 +1,15 @@
 import styles from "./MessageCard.module.scss"
-import {Button, Card, CardActions, CardContent, Slide, Typography, useMediaQuery} from "@mui/material";
+import {Card, CardContent, Typography} from "@mui/material";
 import {getRandomCardColour} from "./colours";
 import React, {useState} from "react";
-import {useSpring, animated, config, Transition} from 'react-spring'
 import ReactCardFlip from "react-card-flip";
+import SingleLanguageMessageCard from "./SingleLanguageMessageCard";
 
 
 export interface MessageProps {
     author: string,
-    message: string // an object containing the message per language [EN, JA]
+    message: string,
+    color: string
 }
 
 export interface MultiLanguageMessageProps {
@@ -22,8 +23,24 @@ export interface JSONMessageEntry {
 }
 
 
+/*
+* Flip card to switch languages
+* */
+export default function MessageCard({author, message}: MultiLanguageMessageProps) {
+    // according to form, at least one language is provided
+    if (message.JA == "")
+        return <SingleLanguageMessageCard author={author} message={message.EN} />
+    else if (message.EN == "")
+        return <SingleLanguageMessageCard author={author} message={message.JA} />
+
+    return <MultiLanguageMessageCard author={author} message={message} />
+
+}
+
 export function MultiLanguageMessageCard({author, message}: MultiLanguageMessageProps) {
     const [showEN, setShowEN] = useState(true);
+    const [colour, setColour] = useState(getRandomCardColour());
+
     return (
         <div onClick={() => setShowEN(!showEN)}>
             <ReactCardFlip
@@ -32,21 +49,18 @@ export function MultiLanguageMessageCard({author, message}: MultiLanguageMessage
                 flipSpeedBackToFront={0.6}
                 flipSpeedFrontToBack={0.6}
             >
-                <MessageCard author={author} message={message.JA}/>
-                <MessageCard author={author} message={message.EN}/>
+                <MessageCardBase author={author} message={message.JA} color={colour}/>
+                <MessageCardBase author={author} message={message.EN} color={colour}/>
             </ReactCardFlip>
         </div>
     );
 }
 
-
-export default function MessageCard({author, message}: MessageProps) {
+export function MessageCardBase({author, message, color}: MessageProps) {
     const cardRef = React.useRef(null);
-    const [colour, setColour] = useState(getRandomCardColour());
-
     return <Card className={styles.card}
                  ref={cardRef}
-                 sx={{background: colour}}>
+                 sx={{background: color}}>
         <CardContent>
             <Typography className={styles.content}>
                 {message}
---
+++ components/MessageCard/SingleLanguageMessageCard.tsx
@@ -0,0 +1,72 @@
+import React, {useState} from "react";
+import {getRandomCardColour} from "./colours";
+import {animated, useSpring} from "react-spring";
+import {Alert, Snackbar} from "@mui/material";
+import {MessageCardBase} from "./MessageCard";
+
+enum PositionContext {
+    left = -10,
+    mid = 0,
+    right = 10
+}
+
+export interface SingleLanguageMessageProps {
+    author: string,
+    message: string
+}
+
+ /*
+* In case only one language is available, this card will reject when the user tempts to click it.
+* */
+export default function SingleLanguageMessageCard({author, message}: SingleLanguageMessageProps) {
+    const [color, setColor] = useState(getRandomCardColour());
+
+    // the relative x value of a card based on its contextual location
+    const [positionState, setPositionState] = useState(PositionContext.mid)
+    const [showSnackBar, setShowSnackBar] = useState(false);
+
+    const styles = useSpring({
+        x: positionState,
+        config: {
+            duration: 100,
+        },
+        onRest: () => {
+            if (positionState == PositionContext.left)
+                setPositionState(PositionContext.right)
+            else if (positionState == PositionContext.right)
+                setPositionState(PositionContext.mid)
+
+        }
+    });
+
+    const handleClick = () => {
+        setPositionState(PositionContext.left)
+        setShowSnackBar(true)
+    }
+
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setShowSnackBar(false);
+    };
+    return <>
+        <Snackbar
+            open={showSnackBar}
+            autoHideDuration={3000}
+            onClose={handleClose}
+        >
+            <Alert onClose={handleClose} severity="info">
+                This message does not have a translation
+                <br/>
+                翻訳はありません。
+            </Alert>
+        </Snackbar>
+        <animated.div style={styles}
+                      onClick={handleClick}
+        >
+            <MessageCardBase author={author} message={message} color={color}/>
+        </animated.div>
+    </>
+}
