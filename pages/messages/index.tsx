1c79af50955b141752e59be40db3f37ae3753856
azuraga <73472335+azuraga@users.noreply.github.com>
2021-11-13 00:07:12 +0100

    added background

--- pages/messages/index.tsx
+++ pages/messages/index.tsx
@@ -1,24 +1,22 @@
 import type { NextPage } from 'next'
-import MessageBoard from "../../components/MessageCard/MessageBoard";
 import MessageCard from "../../components/MessageCard/MessageCard";
-import NextButton from "../../components/Button/NextButton";
-import Footer from "../../components/MainElements/Footer";
 import React from "react";
-import {Grid, Slide} from "@mui/material";
 import message_json from './rei_messages.json'
 import message_friends_json from './rei_messages_friends.json'
 import {JSONMessage} from "../../components/MessageCard/message";
-
+import MessageBoard from "../../components/MessageCard/MessageBoard";
+import {Grid, Slide} from "@mui/material";
+import NextButton from "../../components/Button/NextButton";
+import Footer from "../../components/MainElements/Footer";
 const jsonToJsx = (message: JSONMessage) => <MessageCard rawMessage={message}/>;
 
 const MessagesPage: NextPage = () => {
     const friends = message_friends_json.content.map(jsonToJsx);
     const fans = message_json.content.map(jsonToJsx);
-
     const messages = friends.concat(fans);
 
-    return (<>
-            {/*<Background className={styles.background}/>*/}
+    return (
+        <>
             <Grid container
                   spacing={5}
                   alignItems="center"
@@ -26,23 +24,21 @@ const MessagesPage: NextPage = () => {
                   marginTop="2vh"
                   direction="column"
                   columns={1}
-            >
-                <Grid item>
-                    <Slide direction="down" in={true}>
-                        <img src="MessagePageHeader.svg" alt={"皆のメッセージ"}/>
-                    </Slide>
+                >
+                    <Grid item>
+                        <Slide direction="down" in={true}>
+                            <img src="MessagePageHeader.svg" alt={"皆のメッセージ"}/>
+                        </Slide>
+                    </Grid>
+                    <Grid item>
+                        <MessageBoard messages={messages}/>
+                    </Grid>
+                    <Grid item>
+                        <NextButton href={"/"}/>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <MessageBoard messages={messages}/>
-                </Grid>
-                <Grid item>
-                    <NextButton href={"/"}/>
-                </Grid>
-            </Grid>
             <Footer/>
-
         </>
-
     )
 }
 
