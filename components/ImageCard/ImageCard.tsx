005fb6a4486e38231840c9541252ed8c82516b58
azuraga <73472335+azuraga@users.noreply.github.com>
2021-11-14 14:13:47 +0100

    streamlined ImageCard button functionality with that of MessageCard

--- components/ImageCard/ImageCard.tsx
+++ components/ImageCard/ImageCard.tsx
@@ -1,9 +1,9 @@
 import React, {useState} from "react";
 import {ImageCardBase} from "./ImageCardBase";
 import Backdrop from '@mui/material/Backdrop';
-import Button from '@mui/material/Button';
 import {getRandomCardColour} from "../MessageCard/colours";
 import styles from "./ImageCard.module.scss";
+import {useSpring, animated, config} from "react-spring";
 
 export interface ImageCardProps {
     artist: string,
@@ -12,8 +12,14 @@ export interface ImageCardProps {
 
 export default function ImageCard({artist,image}: ImageCardProps) {
    
-    const  [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
     const [color, setColor] = useState(getRandomCardColour());
+
+    const animation = useSpring({
+        transform: `perspective(600px) rotateY(${open ? 180 : 0}deg)`,
+        opacity: open ? 0 : 1,
+        config: config.default,
+    });
 
     const handleClose = () => {
         setOpen(false);
@@ -21,19 +27,19 @@ export default function ImageCard({artist,image}: ImageCardProps) {
     const handleToggle = () => {
         setOpen(!open);
     }
+
     return(
-    <div>
-        <Button onClick={handleToggle}>
-            <ImageCardBase artist={artist} image={image} color={color}/>
-        </Button>
+    <>
         <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 10 }}
-        open={open}
-        onClick={handleClose}
-      >
-            <img alt={artist} src={image} className={styles.fullImg}/>
-       </Backdrop>
-     </div>
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 10 }}
+            open={open}
+            onClick={handleClose}>
+            <img alt={artist} src={image} className={styles.fullImg} />
+        </Backdrop>
+        <animated.div onClick={handleToggle} style={animation}>
+            <ImageCardBase artist={artist} image={image} color={color}/>
+        </animated.div>
+     </>
     );
     // todo: make use of spring physics so state doesn't need to be used.
     // the relative x value of a card based on its contextual location
