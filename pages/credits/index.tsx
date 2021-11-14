7d21937aa20606edb3b074b011e477ee6eb9a469
azuraga <73472335+azuraga@users.noreply.github.com>
2021-11-14 22:29:36 +0100

    progress on credits

--- pages/credits/index.tsx
+++ pages/credits/index.tsx
@@ -1,56 +1,123 @@
 import type { NextPage } from 'next'
-import {Container, Fade, Grid, Typography} from "@mui/material";
+import {Button, Card, CardActions, CardContent, Container, Fade, Grid, Tooltip, Typography} from "@mui/material";
 import styles from "../../styles/HappyBirthday.module.scss";
 import React from 'react';
 import {DownButtonWithLabel} from "../../components/Button/PreparedButtons";
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import Masonry from "@mui/lab/Masonry";
+import {Box} from "@mui/system";
 
-const Message = () => (
-        <Typography variant="body1" align="center">
-            Credits
-            <br/>
-            Product Owner - Sheep
-            <br/>
-            Lead Developer - Azuraga
-            <br/>
-            Developer - RedMap
-            <br/>
-            Developer - short643
-            <br/>
-            Commissioned artist -  <a href="https://www.twitter.com/coppersketches">@Ninami15</a>
-            <br/>
-        </Typography>
-);
+const cardStyle = {
+    maxWidth: 275,
+    borderRadius: '10px',
+    backgroundColor: '#FCD9D7'
+}
+
+const creditList = [
+    <Card sx={cardStyle} key={0}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Product Owner
+                </Typography>
+                <Typography  color="text.primary">
+                    Sheep
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Tooltip title="@Idle_Sheep">
+                    <Button href="https://twitter.com/Idle_Sheep" startIcon={<TwitterIcon/>}/>
+                </Tooltip>
+            </CardActions>
+        </Card>,
+    <Card sx={cardStyle} key={1}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Lead Developer
+                </Typography>
+                <Typography color="text.primary">
+                    Azuraga
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Tooltip title="@azuragasetsu">
+                    <Button href="https://twitter.com/azuragasetsu" startIcon={<TwitterIcon/>}/>
+                </Tooltip>
+                <Tooltip title="Azuraga Melody">
+                    <Button href="https://www.youtube.com/channel/UC4msLaLAIDy1XTAr-Whw4-g" startIcon={<YouTubeIcon/>}/>
+                </Tooltip>
+            </CardActions>
+        </Card>,
+    <Card sx={cardStyle} key={2}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Developer
+                </Typography>
+                <Typography color="text.primary">
+                    RedMap
+                </Typography>
+            </CardContent>
+            <CardActions>
+            </CardActions>
+        </Card>,
+    <Card sx={cardStyle} key={3}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Developer
+                </Typography>
+                <Typography color="text.primary">
+                    short643
+                </Typography>
+            </CardContent>
+            <CardActions>
+            </CardActions>
+        </Card>,
+    <Card sx={cardStyle} key={4}>
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Commissioned Artist
+                </Typography>
+                <Typography color="text.primary">
+                    Ninami15
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Tooltip title="@coppersketches">
+                    <Button href="https://www.twitter.com/coppersketches" startIcon={<TwitterIcon/>}/>
+                </Tooltip>
+            </CardActions>
+        </Card>
+];
 
 
 const credits: NextPage = () => {
     return (
-            <Grid container
-                  spacing={5}
-                  direction="column"
-                  alignItems="center"
-                  justifyContent="center"
-                  marginTop="2vh"
-            >
-                <Grid item>
-                    <Fade in={true} timeout={3000}>
-                        <div>
-                            <img className={styles.logo} src="/logo.png" alt="Tadano Rei Birthday Project"/>
-                        </div>
-                    </Fade>
-                </Grid>
+        <Box
+             sx={{
+                 gap: 5,
+                 display: 'flex',
+                 justifyContent: 'center',
+                 alignItems:"center",
+                 flexDirection: 'column',
+             }}
+        >
+            <Fade in={true} timeout={3000}>
+                <div>
+                    <img className={styles.logo} src="/logo.png" alt="Tadano Rei Birthday Project"/>
+                </div>
+            </Fade>
 
-                <Grid item>
-                    <Fade in={true} timeout={1500}>
-                        <Container maxWidth="md">
-                            <Message/>
-                        </Container>
-                    </Fade>
-                </Grid>
+            <Typography variant="h5">Credits</Typography>
 
-                <Grid item>
-                    <DownButtonWithLabel href={"/messages"}>back to messages</DownButtonWithLabel>
-                </Grid>
-            </Grid>
+            <Container maxWidth="md">
+                <Masonry columns={3} spacing={3}>
+                    {creditList.map((element, index) => <div key={index}>{element}</div>)}
+                </Masonry>
+            </Container>
+
+
+            <DownButtonWithLabel href={"/messages"}>back to messages</DownButtonWithLabel>
+        </Box>
     )
 
 }
