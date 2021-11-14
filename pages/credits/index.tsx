462d2d71c24c60130185ba9a5858a4e3aefba3ab
azuraga <73472335+azuraga@users.noreply.github.com>
2021-11-14 19:23:36 +0100

    removed unused code

--- pages/credits/index.tsx
+++ pages/credits/index.tsx
@@ -24,29 +24,6 @@ const Message = () => (
 );
 
 
-function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-        width,
-        height
-    };
-}
-
-export function useWindowDimensions() {
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-    useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return windowDimensions;
-}
-
 const credits: NextPage = () => {
     return (
             <Grid container
