76e9eb2f0a1033db02a217a8bb1d18d60dcf2a9b
azuraga <73472335+azuraga@users.noreply.github.com>
2021-11-15 04:49:49 +0100

    fixed font issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,13 @@ export default function MyApp(props: any) {
     return (
         <React.Fragment>
             <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+                <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet"/>
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
                 <title>Tadano Rei Anniversary Project</title>
                 <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
             </Head>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,9 @@ export default class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     {/* PWA primary color */}
+                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+                    <link href="https://fonts.googleapis.com/css2?family=Jua&display=swap" rel="stylesheet"/>
                     <link
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
